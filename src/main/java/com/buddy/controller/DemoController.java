e06dfd5612b657d0f860758ae8a200a1f5ee490a
rosa85 <karolina.steinborn@gmail.com>
2025-05-22 10:42:41 +0200

    first commit

---
+++ src/main/java/com/buddy/controller/DemoController.java
@@ -0,0 +1,12 @@
+package com.buddy.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello, Jack <3";
+    }
+}
